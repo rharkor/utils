5e81febd73017bd73a4451304aef0dfeade6dc68
rharkor <louis@huort.com>
2024-07-30 11:49:09 +0200

    refactor: utils

--- src/index.ts
+++ src/index.ts
@@ -202,64 +202,15 @@ export function stringToColor(text: string | String) {
   return color;
 }
 
-/**
- * Get the time between to dates
- * @param firstDate First date
- * @param secondDate Second date
- * @returns The time between the two dates
- * @example
- * getTimeBetween(new Date("2020-01-01"), new Date("2020-01-02")) // 1 day
- */
-export const getTimeBetween = (firstDate: Date, secondDate: Date) => {
-  // Time difference in milliseconds
-  const timeDiff = Math.abs(firstDate.getTime() - secondDate.getTime()); // in milliseconds
+export function chunk<T>(array: T[], size: number): T[][] {
+  const result: T[][] = [];
 
-  // Define time intervals in milliseconds
-  const intervals = {
-    year: 365.25 * 24 * 60 * 60 * 1000, // Average number of days in a year, including leap years
-    month: 30.44 * 24 * 60 * 60 * 1000, // Average number of days in a month
-    day: 24 * 60 * 60 * 1000,
-    hour: 60 * 60 * 1000,
-    minute: 60 * 1000,
-    second: 1000,
-  };
-
-  // Calculate the number of intervals elapsed
-  const elapsed = {
-    year: Math.floor(timeDiff / intervals.year),
-    month: Math.floor(timeDiff / intervals.month),
-    day: Math.floor(timeDiff / intervals.day),
-    hour: Math.floor(timeDiff / intervals.hour),
-    minute: Math.floor(timeDiff / intervals.minute),
-    second: Math.floor(timeDiff / intervals.second),
-  };
-
-  // Determine the appropriate time interval to display
-  let timeUnit;
-  let timeValue;
-  if (elapsed.year > 0) {
-    timeUnit = "year";
-    timeValue = elapsed.year;
-  } else if (elapsed.month > 0) {
-    timeUnit = "month";
-    timeValue = elapsed.month;
-  } else if (elapsed.day > 0) {
-    timeUnit = "day";
-    timeValue = elapsed.day;
-  } else if (elapsed.hour > 0) {
-    timeUnit = "hour";
-    timeValue = elapsed.hour;
-  } else if (elapsed.minute > 0) {
-    timeUnit = "minute";
-    timeValue = elapsed.minute;
-  } else {
-    timeUnit = "second";
-    timeValue = elapsed.second;
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
   }
 
-  // Construct and return the time elapsed string
-  return `${timeValue} ${timeUnit}${timeValue !== 1 ? "s" : ""}`;
-};
+  return result;
+}
 
 export default {
   sizedDecimal,
@@ -274,5 +225,5 @@ export default {
   camelToSnakeCase,
   snakeToCamelCase,
   stringToColor,
-  getTimeBetween,
+  chunk,
 };
